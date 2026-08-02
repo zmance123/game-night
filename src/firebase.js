276e4a78a51f832f18166f12ec1cfc68514f96f9
Zvonimir Mance <zvonimir.mance@gmail.com>
2026-08-02 19:05:14 +0200

    - firebase setup plus testing catalog  fetch

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Ucitavanje potrebnih funkcija
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+// Firebase konfiguracija
+const firebaseConfig = {
+  apiKey: 'AIzaSyCSn-W0FqmCUcMFlyshjeCvqgLj0HNuyJM',
+  authDomain: 'game-night-8fdf1.firebaseapp.com',
+  projectId: 'game-night-8fdf1',
+  storageBucket: 'game-night-8fdf1.firebasestorage.app',
+  messagingSenderId: '1011095720421',
+  appId: '1:1011095720421:web:accd98bc45008b31645f0c',
+}
+
+// Firebase inicijalizacija
+const app = initializeApp(firebaseConfig)
+
+// Inicijalizacija servisa
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+// Izvoz servisa
+export { auth, db }
--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
     <h1>Catalog</h1>
+    <ul>
+      <li v-for="game in games" :key="game.id">
+        {{ game.title }} |
+        {{ game.genre }} |
+        {{ game.minPlayers }}-{{ game.maxPlayers }} players |
+        {{ game.durationMinutes }} min |
+        {{ game.difficulty }} |
+        {{ game.available ? 'available' : 'borrowed' }} |
+        {{ game.description }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/firebase.js'
+
+const games = ref([])
+
+onMounted(async () => {
+  const snapshot = await getDocs(collection(db, 'games'))
+  games.value = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+})
+</script>
