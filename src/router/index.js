214f9240afa79653228d5d111c17a59b25203abc
Zvonimir Mance <zvonimir.mance@gmail.com>
2026-08-29 15:54:54 +0200

    - 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -49,6 +49,11 @@ const router = createRouter({
             name: 'profile',
             component: () => import('../views/ProfileView.vue'),
         },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: () => import('../views/NotFoundView.vue'),
+        },
     ],
 })
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="text-center py-5">
+        <h1 class="display-4">404</h1>
+        <p class="lead">This page does not exist.</p>
+        <RouterLink class="btn btn-primary" to="/">Back to home</RouterLink>
+    </div>
+</template>
